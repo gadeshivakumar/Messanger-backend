ac904e6148797f768bab386fac077ec397b6029a
gade shiva kumar <shivakumargade955@gmail.com>
2026-01-20 19:39:21 +0530

    cookie

--- middlewares/authorize.js
+++ middlewares/authorize.js
@@ -3,7 +3,7 @@ const jwt=require('jsonwebtoken')
 function authorize(req,res,next){
     
     const token = req.cookies?.token
-
+    console.log(token);
     if(!token){
         return res.status(401).send("no user is found")
     }
