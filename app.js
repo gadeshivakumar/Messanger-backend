89e70f50e44ca312c0dfd127327301100cf62bbc
gade shiva kumar <119879719+gadeshivakumar@users.noreply.github.com>
2026-03-27 11:26:11 +0530

    Enable CORS middleware in app.js

--- app.js
+++ app.js
@@ -18,14 +18,14 @@ connectDB()
 
 const User =require('./models/chatSchema')
 
-// app.use(cors(
-//     {
-//         origin:process.env.ORIGIN,
-//         credentials:true,
-//         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//         allowedHeaders: ["Content-Type", "Authorization"]
-//     }
-// ))
+app.use(cors(
+    {
+        origin:process.env.ORIGIN,
+        credentials:true,
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"]
+    }
+))
 
 app.use(cook())
 
